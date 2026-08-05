d1a085c54ed334e8df5ded75c8aeef3f9fc5aa50
Jugaru Paul <paul.jugaru@student.unitbv.ro>
2026-08-05 23:43:57 +0300

    Multiple table joins exercise

---
+++ SQL_Bootcamp/05_Joins/03_multiple_tables_and_exercises.sql
@@ -0,0 +1,19 @@
+/* Task: Using SalesDB, retrieve a list of all orders, along with the related customer, product,
+and employee details. For each order, display:
+Order ID, Customer's name, Product name, Sales, Price, Sales person's name */
+
+SELECT 
+	o.OrderID,
+	o.Sales,
+	c.FirstName AS CustomerFirstName,
+	c.LastName AS CustomerLastName,
+	p.Product AS ProductName,
+	e.FirstName AS EmployeeFirstName,
+	e.LastName AS EmployeeLastName
+FROM Sales.Orders AS o
+LEFT JOIN Sales.Customers AS c
+ON o.CustomerID = c.CustomerID
+LEFT JOIN Sales.Products AS p
+ON o.ProductID = p.ProductID
+LEFT JOIN Sales.Employees AS e
+ON o.SalesPersonID = e.EmployeeID;
